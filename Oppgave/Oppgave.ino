944a4585f10646b8cb00ab085e198ff91aa46a44
oleleire <oleleire1@gmail.com>
2023-10-12 15:35:47 +0200

    Har endret en god del

--- Oppgave/Oppgave.ino
+++ Oppgave/Oppgave.ino
@@ -1,8 +1,30 @@
+
 const int redLed = 8;
 const int sensorPin = A0;
-int sensorValue = 0; 
-const int unsigned long eventInterval = 1000;
+
+const unsigned long eventInterval = 1000;
 unsigned long previousTime = 0;
+unsigned long currentTime = 0;
+const unsigned long eventInterval2 = 1000;
+unsigned long previousTime2 = 0;
+unsigned long currentTime2 = 0;
+
+const unsigned long eventInterval3 = 1000;
+unsigned long previousTime3 = 0;
+unsigned long currentTime3 = 0;
+
+const unsigned long eventInterval4 = 1000/6;
+unsigned long previousTime4 = 0;
+unsigned long currentTime4 = 0;
+
+int sensorValInn = 0; 
+int sensorValOut = 0;
+int gjennomsnitt = 0;
+int verdi =0;
+int ledState = LOW;
+
+int sensorValList[4]; 
+
 
 
 
@@ -17,22 +39,73 @@ void sensorVerdi(){
 
 unsigned long currentTime = millis();
 
+
 if (currentTime - previousTime >= eventInterval){
-sensorValue = analogRead(sensorPin);
-     Serial.println(sensorValue);
-    
-    previousTime = currentTime; 
+     sensorValInn = analogRead(sensorPin);
+     //Serial.println(sensorValInn);
+     sensorValOut = map (sensorPin, 0, 1023, 0, 200);
+     previousTime = currentTime; 
+     verdi = 0;
+     
+     for (int i=0; i<=4; i++){
+       sensorValList[i] = sensorValOut;
+
+        //Serial.println(sensorValList[i]);
+       verdi =verdi + sensorValList[i];
+    }
+
+
 }
 
 } 
 
+void snittverdi(){
+    unsigned long currentTime2 = millis();
+    if (currentTime2 - previousTime2 >= eventInterval2){
+        gjennomsnitt = (verdi ) /5;
+        Serial.println(gjennomsnitt);
+        previousTime2 = currentTime2; 
+    }
+}
 
+void blink1(){
 
+        unsigned long currentTime3 = millis();
+    if (currentTime3 - previousTime3 >= eventInterval3){
+        previousTime3 = currentTime3; 
+    
+    /*if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+*/
+    // set the LED with the ledState of the variable:
+    digitalWrite(redLed, HIGH);
+}
+}
 
+void blink6(){
+        unsigned long currentTime4 = millis();
+    if (currentTime4 - previousTime4 >= eventInterval4){
+        previousTime4 = currentTime4; 
+        
+    /*if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+    */
+    digitalWrite(redLed, HIGH);
+}
+}
 
 
 void loop(){
 sensorVerdi();
+snittverdi();
+blink1();
+blink6();
 }
 
 
